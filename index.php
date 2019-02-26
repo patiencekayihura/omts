d761e6bc7b5c68e87c5e4b586145963e58c1e397
mwibutsa <mflohost@gmail.com>
2019-02-26 15:44:58 +0200

    Add an image tp home page

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 require_once "templates/header.php"
 ?>
-				
+			<div class="home-header">
+        <img src="pic.jpg" alt="Massage Example Images" class="head-img">
+      </div>	
 <?php
 include "templates/footer.php"
 ?>
